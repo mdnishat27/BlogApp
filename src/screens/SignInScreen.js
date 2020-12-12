89482f6ce5f21a2eaa7457ddf28deec9d2c67f22
mdnishat27 <59754386+mdnishat27@users.noreply.github.com>
2020-12-12 09:51:47 +0600

    loading screen added

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -8,68 +8,82 @@ import firebase from '@react-native-firebase/app';
 import '@react-native-firebase/auth';
 
 import {AuthContext} from '../providers/AuthProvider';
+import Loading from '../components/Loading';
 
 const SignInScreen = (props) => {
   const [Email, setEmail] = useState('');
   const [Password, setPassword] = useState('');
-  return (
-    <AuthContext.Consumer>
-      {(auth) => (
-        <View style={styles.viewStyle}>
-          <Card>
-            <Card.Title>Welcome to My Blog!</Card.Title>
-            <Card.Divider />
-            <Input
-              leftIcon={
-                <FontAwesomeIcon name="envelope" size={24} color="black" />
-              }
-              placeholder="E-mail Address"
-              onChangeText={function (currentInput) {
-                setEmail(currentInput);
-              }}
-            />
+  const [isLoading, setIsLoading] = useState(false);
 
-            <Input
-              placeholder="Password"
-              leftIcon={<Fontisto name="key" size={24} color="black" />}
-              secureTextEntry={true}
-              onChangeText={function (currentInput) {
-                setPassword(currentInput);
-              }}
-            />
+  if (isLoading) {
+    return <Loading />;
+  } else {
+    return (
+      <AuthContext.Consumer>
+        {(auth) => (
+          <View style={styles.viewStyle}>
+            <Card>
+              <Card.Title>Welcome to My Blog!</Card.Title>
+              <Card.Divider />
+              <Input
+                leftIcon={
+                  <FontAwesomeIcon name="envelope" size={24} color="black" />
+                }
+                placeholder="E-mail Address"
+                onChangeText={function (currentInput) {
+                  setEmail(currentInput);
+                }}
+              />
 
-            <Button
-              icon={<FontAwesomeIcon name="sign-in" size={24} color="white" />}
-              title="  Sign In"
-              type="solid"
-              onPress={() => {
-                firebase
-                  .auth()
-                  .signInWithEmailAndPassword(Email, Password)
-                  .then((userCreds) => {
-                    console.log('log in successful');
-                    auth.setIsLoggedIn(true);
-                    auth.setCurrentUser(userCreds.user);
-                    console.log(userCreds.user);
-                  })
-                  .catch((error) => {
-                    alert(error);
-                  });
-              }}
-            />
-            <Button
-              type="clear"
-              icon={<AntDesign name="adduser" size={24} color="dodgerblue" />}
-              title="  Don't have an account?"
-              onPress={function () {
-                props.navigation.navigate('SignUp');
-              }}
-            />
-          </Card>
-        </View>
-      )}
-    </AuthContext.Consumer>
-  );
+              <Input
+                placeholder="Password"
+                leftIcon={<Fontisto name="key" size={24} color="black" />}
+                secureTextEntry={true}
+                onChangeText={function (currentInput) {
+                  setPassword(currentInput);
+                }}
+              />
+
+              <Button
+                icon={
+                  <FontAwesomeIcon name="sign-in" size={24} color="white" />
+                }
+                title="  Sign In"
+                type="solid"
+                onPress={() => {
+                  if (Email != '' && Password != '') {
+                    setIsLoading(true);
+                    firebase
+                      .auth()
+                      .signInWithEmailAndPassword(Email, Password)
+                      .then((userCreds) => {
+                        setIsLoading(false);
+                        console.log('log in successful');
+                        auth.setIsLoggedIn(true);
+                        auth.setCurrentUser(userCreds.user);
+                        console.log(userCreds.user);
+                      })
+                      .catch((error) => {
+                        setIsLoading(false);
+                        alert(error);
+                      });
+                  }
+                }}
+              />
+              <Button
+                type="clear"
+                icon={<AntDesign name="adduser" size={24} color="dodgerblue" />}
+                title="  Don't have an account?"
+                onPress={function () {
+                  props.navigation.navigate('SignUp');
+                }}
+              />
+            </Card>
+          </View>
+        )}
+      </AuthContext.Consumer>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -10,94 +10,107 @@ import '@react-native-firebase/auth';
 import '@react-native-firebase/firestore';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
+import Loading from '../components/Loading';
+
 const SignUpScreen = (props) => {
   const [Name, setName] = useState('');
   const [SID, setSID] = useState('');
   const [Email, setEmail] = useState('');
   const [Password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-  return (
-    <View style={styles.viewStyle}>
-      <Card>
-        <Card.Title>Welcome to My Blog!</Card.Title>
-        <Card.Divider />
-        <Input
-          leftIcon={<Fontisto name="person" size={24} color="black" />}
-          placeholder="Name"
-          onChangeText={function (currentInput) {
-            setName(currentInput);
-          }}
-        />
-        <Input
-          leftIcon={<Ionicons name="ios-school" size={24} color="black" />}
-          placeholder="Student ID"
-          onChangeText={function (currentInput) {
-            setSID(currentInput);
-          }}
-        />
-        <Input
-          leftIcon={<FontAwesome name="envelope" size={24} color="black" />}
-          placeholder="E-mail Address"
-          onChangeText={function (currentInput) {
-            setEmail(currentInput);
-          }}
-        />
+  if (isLoading) {
+    return <Loading />;
+  } else {
+    return (
+      <View style={styles.viewStyle}>
+        <Card>
+          <Card.Title>Welcome to My Blog!</Card.Title>
+          <Card.Divider />
+          <Input
+            leftIcon={<Fontisto name="person" size={24} color="black" />}
+            placeholder="Name"
+            onChangeText={function (currentInput) {
+              setName(currentInput);
+            }}
+          />
+          <Input
+            leftIcon={<Ionicons name="ios-school" size={24} color="black" />}
+            placeholder="Student ID"
+            onChangeText={function (currentInput) {
+              setSID(currentInput);
+            }}
+          />
+          <Input
+            leftIcon={<FontAwesome name="envelope" size={24} color="black" />}
+            placeholder="E-mail Address"
+            onChangeText={function (currentInput) {
+              setEmail(currentInput);
+            }}
+          />
 
-        <Input
-          placeholder="Password"
-          leftIcon={<Fontisto name="key" size={24} color="black" />}
-          secureTextEntry={true}
-          onChangeText={function (currentInput) {
-            setPassword(currentInput);
-          }}
-        />
+          <Input
+            placeholder="Password"
+            leftIcon={<Fontisto name="key" size={24} color="black" />}
+            secureTextEntry={true}
+            onChangeText={function (currentInput) {
+              setPassword(currentInput);
+            }}
+          />
 
-        <Button
-          icon={<AntDesign name="adduser" size={24} color="white" />}
-          title="  Sign Up!"
-          type="solid"
-          onPress={() => {
-            if (Name != '' && SID != '' && Email != '' && Password != '') {
-              firebase
-                .auth()
-                .createUserWithEmailAndPassword(Email, Password)
-                .then((userCreds) => {
-                  userCreds.user.updateProfile({displayName: Name});
-                  firebase
-                    .firestore()
-                    .collection('users')
-                    .doc(userCreds.user.uid)
-                    .set({
-                      name: Name,
-                      sid: SID,
-                      email: Email,
-                    })
-                    .then(() => {
-                      console.log('account created');
-                      alert(userCreds.user.uid);
-                      props.navigation.navigate('SignIn');
-                    })
-                    .catch((error) => {
-                      alert(error);
-                    });
-                })
-                .catch((error) => {
-                  alert(error);
-                });
+          <Button
+            icon={<AntDesign name="adduser" size={24} color="white" />}
+            title="  Sign Up!"
+            type="solid"
+            onPress={() => {
+              if (Name != '' && SID != '' && Email != '' && Password != '') {
+                setIsLoading(true);
+                firebase
+                  .auth()
+                  .createUserWithEmailAndPassword(Email, Password)
+                  .then((userCreds) => {
+                    userCreds.user.updateProfile({displayName: Name});
+                    firebase
+                      .firestore()
+                      .collection('users')
+                      .doc(userCreds.user.uid)
+                      .set({
+                        name: Name,
+                        sid: SID,
+                        email: Email,
+                      })
+                      .then(() => {
+                        setIsLoading(false);
+                        console.log('account created');
+                        alert(userCreds.user.uid);
+                        props.navigation.navigate('SignIn');
+                      })
+                      .catch((error) => {
+                        setIsLoading(false);
+                        alert(error);
+                      });
+                  })
+                  .catch((error) => {
+                    setIsLoading(false);
+                    alert(error);
+                  });
+              }
+            }}
+          />
+          <Button
+            type="clear"
+            icon={
+              <FontAwesomeIcon name="sign-in" size={24} color="dodgerblue" />
             }
-          }}
-        />
-        <Button
-          type="clear"
-          icon={<FontAwesomeIcon name="sign-in" size={24} color="dodgerblue" />}
-          title="  Already have an account?"
-          onPress={function () {
-            props.navigation.navigate('SignIn');
-          }}
-        />
-      </Card>
-    </View>
-  );
+            title="  Already have an account?"
+            onPress={function () {
+              props.navigation.navigate('SignIn');
+            }}
+          />
+        </Card>
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
